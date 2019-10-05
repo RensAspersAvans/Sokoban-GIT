9e4f59646b416008cba9704f820f243a34d78e0c
RensAspersAvans <rca.asper@student.avans.nl>
2019-10-05 17:02:42 +0200

    get next field

--- Sokoban/Model/MazeField.cs
+++ Sokoban/Model/MazeField.cs
@@ -13,9 +13,28 @@ namespace Sokoban
         public MazeField _south { get; set; }
         public MazeField _west { get; set; }
 
-        public void collide()
+        public MazeField findNextField(Directions d)
         {
-
+            if (d == Directions.North)
+            {
+                return _north;
+            }
+            else if(d == Directions.East)
+            {
+                return _east;
+            }
+            else if(d == Directions.South)
+            {
+                return _south;
+            }
+            else if(d == Directions.West)
+            {
+                return _west;
+            }
+            else
+            {
+                return null;
+            }
         }
 
     }
